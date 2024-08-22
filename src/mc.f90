65fa385e050aa30ec2e1f233c090dd8a0a795ed0
osada-yum <osada.yum@gmail.com>
2024-08-22 14:08:21 +0900

    Calculation of the relaxation of magnetization.
    	modified:   src/mc.f90

--- src/mc.f90
+++ src/mc.f90
@@ -1,16 +1,19 @@
 module mc_m
   use, intrinsic :: iso_fortran_env
   implicit none
-  integer(int64), parameter :: nx = 100, ny = 100
+  integer(int64), parameter :: nx = 100, ny = 100, nall = nx * ny
+  real(real64), parameter :: nall_inv = 1d0 / nall
   integer(int32), allocatable :: spins(:, :)
-  real(real64), parameter :: kbt = 2.2d0, beta = 1 / kbt
+  real(real64), parameter :: kbt = 2.3d0, beta = 1 / kbt
   integer(int64), parameter :: dy(4) = [1_int64, 0_int64, -1_int64, 0_int64]
   integer(int64), parameter :: dx(4) = [0_int64, 1_int64, 0_int64, -1_int64]
 contains
   impure subroutine init()
     allocate(spins(nx, ny))
-    spins(:, :) = 1_int32
   end subroutine init
+  impure subroutine set_order_spin()
+    spins(:, :) = 1_int32
+  end subroutine set_order_spin
   impure subroutine metropolis()
     integer(int64) :: i, j
     do i = 1, ny
@@ -55,17 +58,31 @@ contains
             & spins(x, y) = - spins(x, y)
     end if
   end subroutine local_flip
+  pure real(real64) function calc_magne() result(res)
+    res = sum(int(spins, int64)) * nall_inv
+  end function calc_magne
 end module mc_m
 
 program spin_simulation
   use, intrinsic :: iso_fortran_env
   use mc_m
   implicit none
-  integer(int32), parameter :: mcs = 100000
-  integer(int32) :: i
+  integer(int32), parameter :: mcs = 1000, nsample = 100
+  real(real64), allocatable :: magnes(:)
+  integer(int32) :: i, j
   call init()
-  do i = 1, mcs
-     call metropolis()
+  allocate(magnes(mcs), source = 0d0)
+  do j = 1, nsample
+     call set_order_spin()
+     write(error_unit, '(*(g0, 1x))') "sample", j
+     do i = 1, mcs
+        call metropolis()
+        magnes(i) = magnes(i) + calc_magne()
+     end do
   end do
-  write(output_unit, '(*(g0, 1x))') spins(:, :)
+  do i = 1, mcs
+     magnes(i) = magnes(i) / nsample
+     write(output_unit, '(*(g0 ,1x))') i, magnes(i)
+  end do
+  ! write(output_unit, '(*(g0, 1x))') spins(:, :)
 end program spin_simulation
